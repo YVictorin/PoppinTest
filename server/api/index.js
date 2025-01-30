049daf4ee16af77cddde706ef3f3d296772eeb1d
YVictorin <vicyonjou@gmail.com>
2025-01-30 14:23:41 -0500

    update PORT variable

--- server/api/index.js
+++ server/api/index.js
@@ -50,6 +50,6 @@ app.get('/', (req, res) => {
     res.status(200).send({ "message": "Hello world" })
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
