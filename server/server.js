87c14277745a13ca3be165c8c733f6074ac99efc
YVictorin <vicyonjou@gmail.com>
2025-01-30 13:45:52 -0500

    update both package.json files

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ import accountRoute from "./src/routes/account.js"
 
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3001;
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
+// app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 // use routers
 app.use('/', indexRouter);
